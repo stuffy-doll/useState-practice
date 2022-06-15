b81b49289a7d0d834db01817c9acfd3d55a16dfc
Luis Sanchez <97128550+stuffy-doll@users.noreply.github.com>
2022-06-15 08:52:41 -0700

    finished project

--- src/components/UseState/index.js
+++ src/components/UseState/index.js
@@ -3,9 +3,24 @@ import "./UseState.css";
 
 const UseState = () => {
   const [theme, setTheme] = useState("light");
+  const [count, setCount] = useState(0);
   return (
-    <div className={theme}>
+    <div className={`${theme} state`}>
       <h1>UseState Component</h1>
+      <button
+        onClick={() =>
+          theme === "light" ? setTheme("dark") : setTheme("light")
+        }
+      >
+        Theme
+      </button>
+      <h1>{count}</h1>
+      <button onClick={() => setCount((prevCount) => prevCount + 1)}>
+        Increment
+      </button>
+      <button onClick={() => setCount((prevCount) => prevCount - 1)}>
+        Decrement
+      </button>
     </div>
   );
 };
